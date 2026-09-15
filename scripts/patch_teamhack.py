6f06b2848dd71e99f781ac22d46a03daae4a98a8
tayga16 <tayga16@users.noreply.github.com>
2026-09-15 16:36:25 +0400

    Fix Python3 gcc-wrapper and Make 4.3 in patch_teamhack.py

--- scripts/patch_teamhack.py
+++ scripts/patch_teamhack.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 
 print("=== Applying Android 2.3 TLS fix to TeamHackLG kernel ===")
 
@@ -7,93 +7,35 @@ tls_path = os.path.join('kernel_src', 'arch', 'arm', 'include', 'asm', 'tls.h')
 with open(tls_path, 'r', encoding='utf-8') as f:
     tls_content = f.read()
 
-target_tls = """.macro set_tls_v6k, tp, tmp1, tmp2
-\tmcr\tp15, 0, \\tp, c13, c0, 3\t\t@ set TLS register
-\tmov\t\\tmp1, #0
-\tmcr\tp15, 0, \\tmp1, c13, c0, 2\t@ clear user r/w TLS register
-\t.endm"""
+lines = tls_content.splitlines()
+new_lines = []
+patched = False
+for l in lines:
+    new_lines.append(l)
+    if 'mcr' in l and 'c13, c0, 2' in l and not patched:
+        new_lines.append('\tmov\t\\tmp2, #0xffff0fff')
+        new_lines.append('\tstr\t\\tp, [\\tmp2, #-15]\t\t@ set TLS value at 0xffff0ff0')
+        patched = True
 
-repl_tls = """.macro set_tls_v6k, tp, tmp1, tmp2
-\tmcr\tp15, 0, \\tp, c13, c0, 3\t\t@ set TLS register
-\tmov\t\\tmp1, #0
-\tmcr\tp15, 0, \\tmp1, c13, c0, 2\t@ clear user r/w TLS register
-\tmov\t\\tmp2, #0xffff0fff
-\tstr\t\\tp, [\\tmp2, #-15]\t\t@ set TLS value at 0xffff0ff0
-\t.endm"""
-
-if target_tls in tls_content:
-    tls_content = tls_content.replace(target_tls, repl_tls)
-    with open(tls_path, 'w', encoding='utf-8') as f:
-        f.write(tls_content)
-    print("Successfully patched tls.h!")
-else:
-    print("Warning: target_tls not found directly, trying line-by-line replace in tls.h...")
-    lines = tls_content.splitlines()
-    new_lines = []
-    patched = False
-    for l in lines:
-        new_lines.append(l)
-        if 'mcr' in l and 'c13, c0, 2' in l and not patched:
-            new_lines.append('\tmov\t\\tmp2, #0xffff0fff')
-            new_lines.append('\tstr\t\\tp, [\\tmp2, #-15]\t\t@ set TLS value at 0xffff0ff0')
-            patched = True
-    with open(tls_path, 'w', encoding='utf-8') as f:
-        f.write('\n'.join(new_lines) + '\n')
-    print("Patched tls.h via line matching! Result:", patched)
+with open(tls_path, 'w', encoding='utf-8') as f:
+    f.write('\n'.join(new_lines) + '\n')
+print("Patched tls.h! Result:", patched)
 
 # 2. Patch arch/arm/kernel/traps.c
 traps_path = os.path.join('kernel_src', 'arch', 'arm', 'kernel', 'traps.c')
 with open(traps_path, 'r', encoding='utf-8') as f:
     traps_content = f.read()
 
-target_traps = """\tcase NR(set_tls):
-\t\tthread->tp_value = regs->ARM_r0;
-\t\tif (tls_emu)
-\t\t\treturn 0;
-\t\tif (has_tls_reg) {
-\t\t\tasm ("mcr p15, 0, %0, c13, c0, 3"
-\t\t\t\t: : "r" (regs->ARM_r0));
-\t\t} else {
-\t\t\t/*
-\t\t\t * User space must never try to access this directly.
-\t\t\t * Expect your app to break eventually if you do so.
-\t\t\t * The user helper at 0xffff0fe0 must be used instead.
-\t\t\t * (see entry-armv.S for details)
-\t\t\t */
-\t\t\t*((unsigned int *)0xffff0ff0) = regs->ARM_r0;
-\t\t}
-\t\treturn 0;"""
-
-repl_traps = """\tcase NR(set_tls):
-\t\tthread->tp_value = regs->ARM_r0;
-\t\tif (tls_emu)
-\t\t\treturn 0;
-\t\tif (has_tls_reg) {
-\t\t\tasm ("mcr p15, 0, %0, c13, c0, 3"
-\t\t\t\t: : "r" (regs->ARM_r0));
-\t\t}
-\t\t*((unsigned int *)0xffff0ff0) = regs->ARM_r0;
-\t\treturn 0;"""
-
-if target_traps in traps_content:
-    traps_content = traps_content.replace(target_traps, repl_traps)
-    with open(traps_path, 'w', encoding='utf-8') as f:
-        f.write(traps_content)
-    print("Successfully patched traps.c!")
-else:
-    print("Warning: target_traps not found directly, applying fallback replace in traps.c...")
-    # Replace the else block so it's unconditional
-    traps_content = traps_content.replace(
-        '#ifdef CONFIG_NEEDS_SYSCALL_FOR_CMPXCHG',
-        '/* TLS fix for Android 2.3 */\n#ifdef CONFIG_NEEDS_SYSCALL_FOR_CMPXCHG'
-    )
-    # Simple replace:
-    old_block = 'asm ("mcr p15, 0, %0, c13, c0, 3"\n\t\t\t\t: : "r" (regs->ARM_r0));\n\t\t} else {'
-    new_block = 'asm ("mcr p15, 0, %0, c13, c0, 3"\n\t\t\t\t: : "r" (regs->ARM_r0));\n\t\t}\n\t\t{'
+old_block = 'asm ("mcr p15, 0, %0, c13, c0, 3"\n\t\t\t\t: : "r" (regs->ARM_r0));\n\t\t} else {'
+new_block = 'asm ("mcr p15, 0, %0, c13, c0, 3"\n\t\t\t\t: : "r" (regs->ARM_r0));\n\t\t}\n\t\t{'
+if old_block in traps_content:
     traps_content = traps_content.replace(old_block, new_block)
-    with open(traps_path, 'w', encoding='utf-8') as f:
-        f.write(traps_content)
-    print("Patched traps.c via fallback block replacement!")
+    print("Patched traps.c via block replacement!")
+else:
+    print("Warning: old_block not found directly in traps.c")
+
+with open(traps_path, 'w', encoding='utf-8') as f:
+    f.write(traps_content)
 
 # 3. Configure cyanogenmod_m4_defconfig
 defconfig_path = os.path.join('kernel_src', 'arch', 'arm', 'configs', 'cyanogenmod_m4_defconfig')
@@ -104,4 +46,34 @@ def_content += '\n# CONFIG_SECURITY is not set\nCONFIG_DEFAULT_SECURITY_DAC=y\nC
 with open(defconfig_path, 'w', encoding='utf-8') as f:
     f.write(def_content)
 print("Configured cyanogenmod_m4_defconfig successfully!")
+
+# 4. Copy modern python3-compatible gcc-wrapper.py
+gw_src = os.path.join('scripts', 'gcc-wrapper.py')
+gw_dst = os.path.join('kernel_src', 'scripts', 'gcc-wrapper.py')
+if os.path.exists(gw_src):
+    shutil.copy(gw_src, gw_dst)
+    print("Copied Python3-compatible gcc-wrapper.py!")
+
+# 5. Fix GNU Make 4.3 compatibility in Makefile
+mk_compressed = os.path.join('kernel_src', 'arch', 'arm', 'boot', 'compressed', 'Makefile')
+with open(mk_compressed, 'r', encoding='utf-8') as f:
+    mk_c = f.read()
+mk_c = mk_c.replace(
+    '$(obj)/piggy.$(suffix_y).o:  $(obj)/piggy.$(suffix_y) FORCE',
+    '$(obj)/piggy.$(suffix_y).o: $(src)/piggy.$(suffix_y).S $(obj)/piggy.$(suffix_y) FORCE\n\t$(call if_changed_dep,as_o_S)'
+)
+with open(mk_compressed, 'w', encoding='utf-8') as f:
+    f.write(mk_c)
+
+mk_usr = os.path.join('kernel_src', 'usr', 'Makefile')
+with open(mk_usr, 'r', encoding='utf-8') as f:
+    mk_u = f.read()
+mk_u = mk_u.replace(
+    '$(obj)/initramfs_data.o: $(obj)/initramfs_data.cpio$(suffix_y) FORCE',
+    '$(obj)/initramfs_data.o: $(src)/initramfs_data.S $(obj)/initramfs_data.cpio$(suffix_y) FORCE\n\t$(call if_changed_dep,as_o_S)'
+)
+with open(mk_usr, 'w', encoding='utf-8') as f:
+    f.write(mk_u)
+print("Applied GNU Make 4.3 build fixes!")
+
 print("=== PATCHING COMPLETE ===")
